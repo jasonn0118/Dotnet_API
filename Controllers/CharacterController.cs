8bab89553bd8222cec254ac373947e18d2d62420
jasonn0118 <tlswoals2006@gmail.com>
2021-01-12 16:41:39 -0800

    Implemented Migeration, CRUD in Entity Framework core

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -3,6 +3,8 @@ using dotnet_rpg.Models;
 using System.Collections.Generic;
 using System.Linq;
 using dotnet_rpg.Services.CharacterService;
+using System.Threading.Tasks;
+using dotnet_rpg.Dtos.Character;
 
 namespace dotnet_rpg.Controllers
 {
@@ -26,24 +28,66 @@ namespace dotnet_rpg.Controllers
             Apart from that we only have one Get() method in our controller so far, 
             so the web service knows exactly what method is requested.
         */
+        // [HttpGet("GetAll")]
+        // public IActionResult Get()
+        // {
+        //     //NOTE: This enables us to send specific HTTP status codes 
+        //     // back to the client together with the actual data that was requested.
+        //     return Ok(this.characterService.GetAllCharacters());
+        // }
+
+        // [HttpGet("{id}")]
+        // public IActionResult GetSingle(int id)
+        // {
+        //     return Ok(this.characterService.GetCharacterById(id));
+        // }
+
+        // [HttpPost]
+        // public IActionResult AddCharacter(Character newCharacter)
+        // {
+        //     return Ok(this.characterService.AddCharacter(newCharacter));
+        // }
+
         [HttpGet("GetAll")]
-        public IActionResult Get()
+        public async Task<IActionResult> Get()
         {
-            //NOTE: This enables us to send specific HTTP status codes 
-            // back to the client together with the actual data that was requested.
-            return Ok(this.characterService.GetAllCharacters());
+            return Ok(await this.characterService.GetAllCharacters());
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetSingle(int id)
+        public async Task<IActionResult> GetSingle(int id)
         {
-            return Ok(this.characterService.GetCharacterById(id));
+            return Ok(await this.characterService.GetCharacterById(id));
         }
 
         [HttpPost]
-        public IActionResult AddCharacter(Character newCharacter)
+        public async Task<IActionResult> AddCharacter(AddCharacterDto newCharacter)
         {
-            return Ok(this.characterService.AddCharacter(newCharacter));
+            return Ok(await this.characterService.AddCharacter(newCharacter));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCharacter(UpdateCharacterDto updatedCharacter)
+        {
+            ServiceResponse<GetCharacterDto> response = await this.characterService.UpdateCharacter(updatedCharacter);
+            if (response.Data == null)
+            {
+                return NotFound(response);
+            }
+
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            ServiceResponse<List<GetCharacterDto>> response = await this.characterService.DeleteCharacter(id);
+            if (response.Data == null)
+            {
+                return NotFound(response);
+            }
+
+            return Ok(response);
         }
     }
 }
---
+++ Migrations/20210112045737_characterNameChanges.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dotnet_rpg.Migrations
+{
+    public partial class characterNameChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Chracters",
+                table: "Chracters");
+
+            migrationBuilder.RenameTable(
+                name: "Chracters",
+                newName: "Characters");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Characters",
+                table: "Characters",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Characters",
+                table: "Characters");
+
+            migrationBuilder.RenameTable(
+                name: "Characters",
+                newName: "Chracters");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Chracters",
+                table: "Chracters",
+                column: "Id");
+        }
+    }
+}
